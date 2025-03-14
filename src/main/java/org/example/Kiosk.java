c1930939201f1ac2af7a7d6cd50fe39f9d8ab5f3
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2025-03-14 14:37:52 +0000

    added classes

--- src/main/java/org/example/Kiosk.java
+++ src/main/java/org/example/Kiosk.java
@@ -1,6 +1,26 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Kiosk extends JFrame {
+
+    private int HEIGHT = 100;
+    private int WIDTH = 100;
+
+    public Kiosk(){
+        this.initialising();
+
+
+    }
+
+    private void initialising(){
+        this.setTitle("Kiosk Ordering System");
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
+
+        this.setPreferredSize(new Dimension(100, 100));
+    }
+
 }
--- src/main/java/org/example/Restaurant.java
+++ src/main/java/org/example/Restaurant.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Restaurant {
     public static void main(String[] args) {
-
+        Kiosk kiosk = new Kiosk();
     }
 }
