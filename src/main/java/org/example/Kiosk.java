060a22b13f2c73abd32d05473e92140f492488c2
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2025-03-14 18:10:11 +0000

    added background

--- src/main/java/org/example/Kiosk.java
+++ src/main/java/org/example/Kiosk.java
@@ -13,7 +13,6 @@ public class Kiosk extends JFrame {
     public Kiosk(){
         this.initialising();
 
-
     }
 
     private void initialising(){
@@ -24,12 +23,21 @@ public class Kiosk extends JFrame {
 
         this.setSize(new Dimension(WIDTH, HEIGHT));
         this.setLocation(Xaxis, Yaxis);
-        this.getContentPane().setBackground(Color.WHITE);
+        this.getContentPane().setBackground(Color.RED);
 
         this.setVisible(true);
     }
 
+    public void paint(Graphics g)
+    {
+        Image image = new ImageIcon("resource\\background.png").getImage();
+        Image icon = new ImageIcon("resource\\icon.png").getImage();
+        Graphics2D g2d = (Graphics2D) g;
+        
+        g2d.drawImage(image, 0, 0, null);
+        g2d.drawImage(icon, 170, 330, null);
 
+    }
 
 
 }
