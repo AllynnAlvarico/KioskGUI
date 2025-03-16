99e1287c174d5fdcebbcc81dc46f10e2cd559a9f
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2025-03-16 18:39:56 +0000

    made scrolling faster

--- src/main/java/org/example/Kiosk.java
+++ src/main/java/org/example/Kiosk.java
@@ -14,10 +14,10 @@ public class Kiosk extends JFrame implements ActionListener {
     private int Yaxis = 100;
     private int Xaxis = 500;
     private JButton start, confirmButton, cancelButton;
+    JScrollPane scrollPane;
 
     public Kiosk(){
         this.initialising();
-
     }
 
     private void initialising(){
@@ -71,17 +71,33 @@ public class Kiosk extends JFrame implements ActionListener {
         mainPanel.setSize(WIDTH, HEIGHT);
 
         JPanel headerPanel = new JPanel();
-        JPanel middlePanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 15, 15));
+        JPanel middlePanel = new JPanel();
         JPanel bottomPanel = new JPanel(new FlowLayout());
 
         // Settings the header panel
         JLabel headerLabel = new JLabel("FOOD MENU");
-//        headerLabel.setPreferredSize(new Dimension(100,0));
         headerLabel.setBorder(BorderFactory.createEmptyBorder(11, 0, 0, 0));
         headerLabel.setFont(new Font("New Time Roman", Font.BOLD, 18));
         headerPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT - 640));
         headerPanel.setBackground(Color.LIGHT_GRAY);
         headerPanel.add(headerLabel);
+
+        int numItems = 0;
+        for (int i = 0; i < 10; i++) {
+            numItems = i; //getting the total number of panel
+            JPanel foodPanel = FoodPanel(null, "Burger", 3.50);
+            middlePanel.add(foodPanel);
+        }
+
+        //re-calculate the size when the number of panel increase
+        int rows = (int) Math.ceil(numItems / 4.0);
+        int panelHeight = rows * 235;
+
+        middlePanel.setPreferredSize(new Dimension(WIDTH - 25, panelHeight));
+
+        scrollPane = new JScrollPane(middlePanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(20);
 
         //setting the button and customize it with the size and color
         cancelButton = new JButton("Cancel");
@@ -98,16 +114,6 @@ public class Kiosk extends JFrame implements ActionListener {
         confirmButton.setBackground(Color.WHITE);
         confirmButton.setPreferredSize(new Dimension(220,40));
 
-        for (int i = 0; i < 9; i++) {
-            JPanel foodPanel = FoodPanel(null, "Burger", 3.50);
-            middlePanel.add(foodPanel);
-        }
-
-        JScrollPane scrollPane = new JScrollPane(middlePanel);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setPreferredSize(new Dimension(WIDTH, HEIGHT - 200));
-
         bottomPanel.setBackground(Color.lightGray);
         bottomPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT - 600));
         bottomPanel.add(cancelButton);
@@ -117,9 +123,6 @@ public class Kiosk extends JFrame implements ActionListener {
         mainPanel.add(headerPanel, BorderLayout.NORTH);
         mainPanel.add(scrollPane, BorderLayout.CENTER);
         mainPanel.add(bottomPanel, BorderLayout.SOUTH);
-//
-//        JScrollPane scrollPane = new JScrollPane(middlePanel);
-//        this.getContentPane().add(scrollPane, BorderLayout.CENTER);
 
         cancelButton.addActionListener(this);
         confirmButton.addActionListener(this);
@@ -128,14 +131,15 @@ public class Kiosk extends JFrame implements ActionListener {
     }
 
     private JPanel FoodPanel(Image image, String name, Double price) {
-        JPanel foodPanel = new JPanel(new BorderLayout());
-        foodPanel.setBorder(BorderFactory.createEmptyBorder(11, 5, 5, 0));
+        JPanel foodPanel = new JPanel();
+        foodPanel.setLayout(new BoxLayout(foodPanel,BoxLayout.Y_AXIS));
+        foodPanel.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
 
         JPanel imagePanel = new JPanel(new FlowLayout());;
         if (image != null) {
             imagePanel.setPreferredSize(new Dimension(WIDTH / 4, HEIGHT - 560));
         } else {
-            imagePanel.setPreferredSize(new Dimension(WIDTH / 4, HEIGHT - 560));
+            imagePanel.setPreferredSize(new Dimension(WIDTH / 4, HEIGHT - 580));
             imagePanel.setBackground(Color.gray);
         }
 
@@ -181,6 +185,5 @@ class BackgroundPanel extends JPanel {
         Image icon = new ImageIcon("resource\\icon.png").getImage();
         g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
         g.drawImage(icon, 160, 330, this);
-
     }
 }
